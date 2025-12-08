6d7072b90c2780e344cbee2396da603b297deb6d
PC <seuemail@dominio.com>
2025-12-08 08:48:27 -0300

    chore: Configuração de ambiente/Página inicial

---
+++ frontend/next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  reactCompiler: true, // habilita o React Compiler
+};
+
+export default nextConfig;
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,37 @@
+import styles from './styles.module.scss'
+import Image from 'next/image'
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <main className={styles.container}>
+      <div className={styles.card}>
+
+        <Image 
+          src="/logoCoraxy1.png" 
+          alt="Logo Coraxy"
+          width={200}
+          height={200}
+          className={styles.logo}
+        />
+
+        <h1>Documentação de serviços</h1>
+
+        <p className={styles.subtitle}>
+          Escolha uma ação para continuar
+        </p>
+
+        <div className={styles.actions}>
+          <Link href="/criar" className={styles.primaryBtn}>
+            Criar documentação
+          </Link>
+
+          <Link href="/listar" className={styles.secondaryBtn}>
+            Ver documentação
+          </Link>
+        </div>
+
+      </div>
+    </main>
+  );
+}
