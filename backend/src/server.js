91c91cfe4e8cda5c533053066bd27d0514a40ca7
PC <seuemail@dominio.com>
2026-01-06 14:06:57 -0300

    fix: corrige __dirname para ES module

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,21 +2,32 @@ import express from "express";
 import cors from "cors";
 import prismaClient from "./prismaClient.js";
 import "dotenv/config";
+
 import documentacaoRoutes from "./routes/documentacaoRoutes.js";
-import erpRoutes from "./routes/erpRoutes.js"
-import servicoRoutes from "./routes/servicoRoutes.js"
+import erpRoutes from "./routes/erpRoutes.js";
+import servicoRoutes from "./routes/servicoRoutes.js";
 import servicoDesejadoRoutes from "./routes/servicoDesejadoRoutes.js";
-import sistemaRoutes from "./routes/sistemaRoutes.js"
+import sistemaRoutes from "./routes/sistemaRoutes.js";
 import nomeServicoRoutes from "./routes/nomeServicoRoutes.js";
-import empresaRoutes from './routes/empresaRoutes.js'
+import empresaRoutes from "./routes/empresaRoutes.js";
+
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
+// uploads funcionando em ES Module
+app.use(
+  "/uploads",
+  express.static(path.resolve(__dirname, "..", "uploads"))
+);
+
 app.use("/api", documentacaoRoutes);
 app.use("/api", erpRoutes);
 app.use("/api", servicoRoutes);
@@ -24,7 +35,6 @@ app.use("/api", servicoDesejadoRoutes);
 app.use("/api", sistemaRoutes);
 app.use("/api", nomeServicoRoutes);
 app.use("/api", empresaRoutes);
-
 
 const PORT = process.env.PORT || 3001;
 
